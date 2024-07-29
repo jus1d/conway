b5597f33d0e7ef26362fe5aae8f2991c9717b128
jus1d <artfa63@gmail.com>
2024-07-29 23:08:09 +0400

    chore: Update quine

--- quine.rs
+++ quine.rs
@@ -1,12 +1,10 @@
-fn main() {
-    let src = "fn main() {\n    let src = \"?\";\n\n    for i in 0..src.len() {\n        if let Some(ch) = src.chars().nth(i) {\n            match ch as u8 {\n                63u8 => {\n                    for j in 0..src.len() {\n                        if let Some(ch) = src.chars().nth(j) {\n                            match ch {\n                                '\\n' => print!(\"\\\\n\"),\n                                '\"' => print!(\"\\\\\\\"\"),\n                                '\\\\' => print!(\"\\\\\\\\\"),\n                                _ => print!(\"{}\", ch),\n                            }\n                        }\n                    }\n                },\n                _ => print!(\"{}\", ch),\n            }\n        }\n    }\n}\n";
-
-    for i in 0..src.len() {
-        if let Some(ch) = src.chars().nth(i) {
+fn print_source_code(source: &str) {
+    for i in 0..source.len() {
+        if let Some(ch) = source.chars().nth(i) {
             match ch as u8 {
                 63u8 => {
-                    for j in 0..src.len() {
-                        if let Some(ch) = src.chars().nth(j) {
+                    for j in 0..source.len() {
+                        if let Some(ch) = source.chars().nth(j) {
                             match ch {
                                 '\n' => print!("\\n"),
                                 '"' => print!("\\\""),
@@ -20,4 +18,10 @@ fn main() {
             }
         }
     }
+}
+
+fn main() {
+    let src = "fn print_source_code(source: &str) {\n    for i in 0..source.len() {\n        if let Some(ch) = source.chars().nth(i) {\n            match ch as u8 {\n                63u8 => {\n                    for j in 0..source.len() {\n                        if let Some(ch) = source.chars().nth(j) {\n                            match ch {\n                                '\\n' => print!(\"\\\\n\"),\n                                '\"' => print!(\"\\\\\\\"\"),\n                                '\\\\' => print!(\"\\\\\\\\\"),\n                                _ => print!(\"{}\", ch),\n                            }\n                        }\n                    }\n                },\n                _ => print!(\"{}\", ch),\n            }\n        }\n    }\n}\n\nfn main() {\n    let src = \"?\";\n\n    print_source_code(src);\n}\n";
+
+    print_source_code(src);
 }
